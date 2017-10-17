a499e8b8b5920a2355273b1c1ea7c2b878400f50
m70 <m70@m70-VB>
2017-10-17 17:11:09 +0200

    Versió 0.4-B del projecte prjava43benitez18. Branca master modificada.

--- src/prjava43benitez18/Prjava43benitez18.java
+++ src/prjava43benitez18/Prjava43benitez18.java
@@ -1,6 +1,7 @@
 package prjava43benitez18;
 import java.awt.*;        // using AWT containers and components
-import java.awt.event.*;  // using AWT events and listener interfaces
+import java.awt.event.*;
+import java.net.*;// using AWT events and listener interfaces
  
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class Prjava43benitez18 extends Frame
